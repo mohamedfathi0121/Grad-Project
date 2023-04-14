b1596ec89f9a908a001d5583bf772e16d34a3241
aymon <muh.aymanzayed@gmail.com>
2023-04-14 07:28:55 +0100

    Make The Link Active When Clicked and some changes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ import { dropDownList } from "./dropdownlist.js";
 dropDownList();
 
 // *Code Your Js Here //
+
+// Make The Link Active When Clicked
+
+let currentLocation = location.href;
+let links = document.querySelectorAll("nav a");
+
+links.forEach((ele) => {
+	if (ele.href === currentLocation) {
+		ele.classList.add("active");
+	}
+});
