7c628fe35a2f2ca7f261ff942755a3d7ebd05217
Mohamed Ayman <67555382+MuhAymanZ@users.noreply.github.com>
2023-05-04 04:12:56 +0200

    edit on add member & meetings

--- add_member.php
+++ add_member.php
@@ -14,7 +14,7 @@ if (session_status() === PHP_SESSION_NONE)
     ?>
 
 <body dir="rtl">
-    <?php
+  <?php
     Headers();
     Nav();
     if (is_admin()):
@@ -29,24 +29,26 @@ if (session_status() === PHP_SESSION_NONE)
         <div class="col">
 
           <div class="row">
-            <h4>الاسم بالكامل</h4><input type="text" name="name" placeholder="الاسم بالكامل" required/>
+            <h4>الاسم بالكامل</h4><input type="text" name="name" placeholder="الاسم بالكامل" required />
           </div>
           <div class="row">
             <h4>النوع</h4>
-              ذكر<input type="radio" value="M" name="gender" required>
-              أنثى<input type="radio" value="F" name="gender" required>
+            <div class="row">
+              <h4>ذكر</h4><input type="radio" value="M" name="gender" required>
+              <h4>أنثى</h4><input type="radio" value="F" name="gender" required>
+            </div>
           </div>
           <div class="row">
-            <h4>البريد الالكتروني</h4><input type="email" name="email" placeholder="البريد الالكتروني" required/>
+            <h4>البريد الالكتروني</h4><input type="email" name="email" placeholder="البريد الالكتروني" required />
           </div>
           <div class="row">
-            <h4>كلمة السر</h4><input type="password" name="password" placeholder="كلمة السر" required/>
+            <h4>كلمة السر</h4><input type="password" name="password" placeholder="كلمة السر" required />
           </div>
-<!--          <div class="row">-->
-<!--            <h4>رقم التليفون</h4><input type="text" placeholder="رقم التليفون" />-->
-<!--          </div>-->
+          <!--          <div class="row">-->
+          <!--            <h4>رقم التليفون</h4><input type="text" placeholder="رقم التليفون" />-->
+          <!--          </div>-->
           <div class="row">
-            <h4>المسمى الوظيفي</h4><input type="text" name="job_title" placeholder="المسمى الوظيفي" required/>
+            <h4>المسمى الوظيفي</h4><input type="text" name="job_title" placeholder="المسمى الوظيفي" required />
           </div>
           <div class="row">
             <h4>الفئة الوظيفية</h4>
@@ -61,8 +63,8 @@ if (session_status() === PHP_SESSION_NONE)
                 while ($job_types_row = $job_types_result->fetch_assoc())
                 {
                     ?>
-                    <option value="<?=$job_types_row['job_type_id']?>"><?=$job_types_row["job_type_name"]?></option>
-                    <?php
+                <option value="<?=$job_types_row['job_type_id']?>"><?=$job_types_row["job_type_name"]?></option>
+                <?php
                 }
                 $job_types_stmt->close();
                 ?>
@@ -73,8 +75,8 @@ if (session_status() === PHP_SESSION_NONE)
             <h4>الدرجة الوظيفية</h4>
             <div class="select-basic">
               <select name="job_rank" required>
-                  <option value="">اختر</option>
-                  <?php
+                <option value="">اختر</option>
+                <?php
                   // Get All Job Types
                   $job_ranks_stmt = $conn->prepare("SELECT * FROM p39_job_rank");
                   $job_ranks_stmt->execute();
@@ -82,8 +84,8 @@ if (session_status() === PHP_SESSION_NONE)
                   while ($job_ranks_row = $job_ranks_result->fetch_assoc())
                   {
                       ?>
-                      <option value="<?=$job_ranks_row['job_rank_id']?>"><?=$job_ranks_row["job_rank_name"]?></option>
-                      <?php
+                <option value="<?=$job_ranks_row['job_rank_id']?>"><?=$job_ranks_row["job_rank_name"]?></option>
+                <?php
                   }
                   $job_ranks_stmt->close();
                   ?>
@@ -94,8 +96,8 @@ if (session_status() === PHP_SESSION_NONE)
             <h4>القسم العلمي</h4>
             <div class="select-basic">
               <select name="department" required>
-                  <option value="">اختر</option>
-                  <?php
+                <option value="">اختر</option>
+                <?php
                   // Get All Job Types
                   $departments_stmt = $conn->prepare("SELECT * FROM p39_department");
                   $departments_stmt->execute();
@@ -103,8 +105,8 @@ if (session_status() === PHP_SESSION_NONE)
                   while ($departments_row = $departments_result->fetch_assoc())
                   {
                       ?>
-                      <option value="<?=$departments_row['department_id']?>"><?=$departments_row["department_name"]?></option>
-                      <?php
+                <option value="<?=$departments_row['department_id']?>"><?=$departments_row["department_name"]?></option>
+                <?php
                   }
                   $departments_stmt->close();
                   ?>
@@ -129,7 +131,7 @@ if (session_status() === PHP_SESSION_NONE)
                   رفع صورة
                   <i class="fa-solid fa-upload"></i>
                   <input id="up1" type="file" class="upload-button" name="member_picture[]"
-                         accept="image/png, image/gif, image/jpeg" />
+                    accept="image/png, image/gif, image/jpeg" />
                 </label>
               </div>
               <div class="file-list"></div>
