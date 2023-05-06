4d240a237362e2780915fb33dceab285ceaa6768
mofathi <mohamedfathinew2023@gmail.com>
2023-05-06 05:22:11 +0300

    المرفقات

---
+++ subject_attachment.php
@@ -0,0 +1,91 @@
+<?php
+require_once "db.php";
+require_once "functions.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    Head("مرفقات الموضوعات");
+    ?>
+
+<body dir="rtl">
+  <?php
+        Headers();
+        Nav();
+        ?>
+
+
+  <main class="attachment-content">
+    <div class="container">
+      <!-- عنوان الصفحة -->
+      <div class="title">
+        <h1>المرفقات الخاصة بموضوع رقم {2}</h1>
+      </div>
+
+      <main id="empty" class="empty">
+        <h4>لا يوجد ملفات الان</h4>
+      </main>
+
+      <div class="attatchement-box">
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 2 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 3 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 4 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+        <div class="col">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
+          <button class="btn-basic">حذف</button>
+
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+    <!-- اضافة ملف -->
+    <div class="upload add-attachment-subject">
+      <div class="btn-basic">
+        <label for="up1">
+          رفع مرفق
+          <i class="fa-solid fa-upload"></i>
+          <input id="up1" type="file" class="upload-button" multiple />
+        </label>
+      </div>
+      <div class="file-list"></div>
+    </div>
+  </main>
+
+  <?php
+        footer();
+        ?>
+
+  <!-- Js Scripts and Plugins -->
+  <script type="module" src="./js/main.js"></script>
+
+  <!-- font Awesome -->
+  <script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
