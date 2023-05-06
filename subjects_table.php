af498125a276a3ea4641156aad1bfbb203744d11
Mohamed Ayman <67555382+MuhAymanZ@users.noreply.github.com>
2023-05-07 01:34:10 +0200

    creation of subject tables

---
+++ subjects_table.php
@@ -0,0 +1,37 @@
+<?php
+require_once "db.php";
+require_once "functions.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    Head("الأعضاء");
+    ?>
+
+<body dir="rtl">
+  <?php
+        Headers();
+        Nav();
+        ?>
+
+
+  <main>
+    <div class="container">
+
+    </div>
+  </main>
+
+
+
+  <?php
+        footer();
+        ?>
+
+  <!-- Js Scripts and Plugins -->
+  <script type="module" src="./js/main.js"></script>
+
+  <!-- font Awesome -->
+  <script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
