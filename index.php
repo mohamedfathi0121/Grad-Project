5d9e532bbf3b41a6310a7cd051b5e8de20e69d42
Mohamed Ayman <67555382+MuhAymanZ@users.noreply.github.com>
2023-05-07 07:10:40 +0200

    جدول الاعمال خلص

--- index.php
+++ index.php
@@ -9,82 +9,82 @@ Head("الصفحة الرئيسية");
 ?>
 
 <body dir="rtl">
-<?php
+  <?php
 Headers();
 Nav();
 ?>
 
-<!-- main content of the page -->
-<main class="main-page reports-content">
+  <!-- main content of the page -->
+  <main class="main-page reports-content">
     <div class="container">
-        <!-- عنوان الصفحة -->
-        <div class="title">
-            <h1> الصفحة الرئيسية<h1>
-        </div>
-        <?php
+      <!-- عنوان الصفحة -->
+      <div class="title">
+        <h1> الصفحة الرئيسية<h1>
+      </div>
+      <?php
         if ($_SESSION["admin"])
         {
             ?>
-            <div class="box">
-                <div class="row">
-                    <div class="col">
-                        <a href="meetings.php" class="btn-basic">المجالس</a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <a href="formation.php" class="btn-basic">التشكيلات السنوية</a>
-                    </div>
-                    <div class="col">
-                        <a href="members.php" class="btn-basic">الأعضاء</a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col"><a href="#" class="btn-basic">الموضوعات</a></div>
-                    <div class="col"><a href="executive_decisions.php" class="btn-basic">القرارات التنفيذية</a></div>
-                </div>
-            </div>
-            <?php
+      <div class="box">
+        <div class="row">
+          <div class="col">
+            <a href="formation.php" class="btn-basic">التشكيلات السنوية</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <a href="meetings.php" class="btn-basic">المجالس</a>
+          </div>
+          <div class="col">
+            <a href="members.php" class="btn-basic">الأعضاء</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col"><a href="#" class="btn-basic">الموضوعات</a></div>
+          <div class="col"><a href="executive_decisions.php" class="btn-basic">القرارات التنفيذية</a></div>
+        </div>
+      </div>
+      <?php
         }
         else
         {
             ?>
-            <div class="box">
-                <div class="row">
-                    <div class="col">
-                        <a href="meetings.php" class="btn-basic">المجالس</a>
-                    </div>
-                    <div class="col">
-                        <a href="#" class="btn-basic">الموضوعات</a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <a href="" class="btn-basic">جدول الأعمال</a>
+      <div class="box">
+        <div class="row">
+          <div class="col">
+            <a href="meetings.php" class="btn-basic">المجالس</a>
+          </div>
+          <div class="col">
+            <a href="#" class="btn-basic">الموضوعات</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <a href="" class="btn-basic">جدول الأعمال</a>
 
-                    </div>
-                    <div class="col">
-                        <a href="" class="btn-basic">التقارير</a>
+          </div>
+          <div class="col">
+            <a href="" class="btn-basic">التقارير</a>
 
-                    </div>
-                </div>
+          </div>
+        </div>
 
-            </div>
-            <?php
+      </div>
+      <?php
         }
         ?>
 
     </div>
-</main>
-<?php
+  </main>
+  <?php
 footer();
 ?>
 
-<!-- Js Scripts and Plugins -->
-<script type="module" src="./js/main.js"></script>
+  <!-- Js Scripts and Plugins -->
+  <script type="module" src="./js/main.js"></script>
 
-<!-- font Awesome -->
-<script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
+  <!-- font Awesome -->
+  <script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -103,24 +103,6 @@ allCheckBoxes.forEach((check) => {
 	});
 });
 
-// Toggle Password to Text
-
-const inputPassword = document.querySelector("input[type='password']");
-
-const showPasswordIcon = document.querySelector(".fa-eye-slash");
-
-showPasswordIcon.addEventListener("click", () => {
-	if (inputPassword.type === "password") {
-		inputPassword.type = "text";
-		showPasswordIcon.classList.remove("fa-eye-slash");
-		showPasswordIcon.classList.add("fa-eye");
-	} else {
-		inputPassword.type = "password";
-
-		showPasswordIcon.classList.add("fa-eye-slash");
-		showPasswordIcon.classList.remove("fa-eye");
-	}
-});
 console.log("AYMAN");
 
 // Links Toggler
