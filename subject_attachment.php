162b6e448e2825db22b66f40cb16398620fc09ba
Mohamed Ayman <67555382+MuhAymanZ@users.noreply.github.com>
2023-05-11 00:19:09 +0200

    dialog

--- subject_attachment.php
+++ subject_attachment.php
@@ -9,81 +9,82 @@ require_once "functions.php";
     ?>
 
 <body dir="rtl">
-    <?php
+  <?php
         Headers();
         Nav();
         ?>
 
 
-    <main class="attachment-content">
-        <div class="container">
-            <!-- عنوان الصفحة -->
-            <div class="title">
-                <h1>المرفقات الخاصة بموضوع رقم {2}</h1>
-            </div>
+  <main class="attachment-content">
+    <div class="container">
+      <!-- عنوان الصفحة -->
+      <div class="title">
+        <h1>المرفقات الخاصة بموضوع رقم {2}</h1>
+      </div>
 
-            <main id="empty" class="empty deactive">
-                <h4>لا يوجد ملفات الان</h4>
-            </main>
+      <main id="empty" class="empty deactive">
+        <h4>لا يوجد ملفات الان</h4>
+      </main>
 
-            <div class="attatchement-box">
-                <div class="col">
-                    <img src="images/icons/iconpdf.svg" alt="" class="attachment-picture">
-                    <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
-                    <!-- this is not delete button -->
-                    <button data-open-modal class="btn-basic">حذف</button>
-                    <dialog data-modal>
-                        <h4>هل حقا تريد مسح الملف 1</h4>
-                        <form>
-                            <!-- this is the delete code -->
-                            <button class="btn-basic">نعم</button>
-                            <button type="submit" formmethod="dialog" class="btn-basic">لا</button>
-                        </form>
-                    </dialog>
-                </div>
-                <div class="col">
-                    <img src="images/icons/iconimage.svg" alt="" class="attachment-picture">
-                    <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
-                    <button data-open-modal class="btn-basic">حذف</button>
-                    <dialog data-modal>
-                        <h4>هل حقا تريد حذف الملف 2</h4>
-                        <form>
-                            <button class="btn-basic">نعم</button>
-                            <button type="submit" formmethod="dialog" class="btn-basic">لا</button>
-                        </form>
-                    </dialog>
-                </div>
+      <div class="attatchement-box">
+        <div class="col">
+          <img src="images/icons/iconpdf.svg" alt="" class="attachment-picture">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
+          <!-- this is not delete button -->
+          <button data-open-modal class="btn-basic">حذف</button>
+          <dialog data-modal>
+            <h4>هل حقا تريد مسح الملف 1</h4>
+            <form>
+              <!-- this is the delete code -->
+              <button class="btn-basic">نعم</button>
+              <button type="submit" formmethod="dialog" class="btn-basic">لا</button>
+            </form>
+          </dialog>
+        </div>
+        <div class="col">
+          <img src="images/icons/iconimage.svg" alt="" class="attachment-picture">
+          <a href="#">مرفق مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1مرفق 1 1</a>
+          <button data-open-modal class="btn-basic">حذف</button>
+          <dialog data-modal>
+            <form action="">
+              <h4>هل حقا تريد حذف الملف 2</h4>
 
-            </div>
-
-
+              <button class="btn-basic">نعم</button>
+              <button data-close-modal formmethod="dialog" class="btn-basic">لا</button>
+            </form>
+          </dialog>
         </div>
 
+      </div>
 
 
-        <!-- اضافة ملف -->
-        <div class="upload add-attachment-subject">
-            <div class="btn-basic">
-                <label for="up1">
-                    رفع مرفق
-                    <i class="fa-solid fa-upload"></i>
-                    <input id="up1" type="file" class="upload-button" multiple />
-                </label>
-            </div>
-            <div class="file-list"></div>
-        </div>
-    </main>
+    </div>
 
 
-    <?php
+
+    <!-- اضافة ملف -->
+    <div class="upload add-attachment-subject">
+      <div class="btn-basic">
+        <label for="up1">
+          رفع مرفق
+          <i class="fa-solid fa-upload"></i>
+          <input id="up1" type="file" class="upload-button" multiple />
+        </label>
+      </div>
+      <div class="file-list"></div>
+    </div>
+  </main>
+
+
+  <?php
         footer();
         ?>
 
-    <!-- Js Scripts and Plugins -->
-    <script type="module" src="./js/main.js"></script>
+  <!-- Js Scripts and Plugins -->
+  <script type="module" src="./js/main.js"></script>
 
-    <!-- font Awesome -->
-    <script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
+  <!-- font Awesome -->
+  <script src="https://kit.fontawesome.com/eb7dada2f7.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
