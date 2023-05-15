eb539457a7a2de3882fc063c3c91982674a59373
Mahmoud Badr <mahmoud.badr7@hotmail.com>
2023-05-15 19:29:45 +0300

    Added members search
    Signed-off-by: Mahmoud Badr <mahmoud.badr7@hotmail.com>

--- members.php
+++ members.php
@@ -18,7 +18,7 @@ if(session_status() === PHP_SESSION_NONE)
         Headers();
         if(is_admin()):
             Nav();
-            ?>
+            SearchBar(); ?>
             <!-- *Main Members Page Content  -->
             <!-- !Admin Apperance -->
             <!-- *Add "deactive" to Class Here ↓↓ To Test-->
@@ -83,14 +83,15 @@ if(session_status() === PHP_SESSION_NONE)
                     $users_stmt = $conn->prepare("SELECT * FROM p39_users");
                     $users_stmt->execute();
                     $users_result = $users_stmt->get_result();
-                    if ($users_result->num_rows == 0):
+                    $users_exist = $users_result->num_rows > 0;
+                    if (!$users_exist && empty($_GET)):
                         ?>
                         <div class="members">
                             <main id="empty" class="empty-member">
                                 <h4>لا يوجد أعضاء الآن</h4>
                             </main>
                         <?php
-                    else:
+                    elseif ($users_exist && empty($_GET)):
                         $n = 1;
                         while ($users_row = $users_result->fetch_assoc())
                         {
@@ -143,8 +144,81 @@ if(session_status() === PHP_SESSION_NONE)
                             <?php
                             $n += 1;
                         }
-                    endif;
-                        ?>
+                    elseif (!empty($_GET["search"])):
+                        switch ($_GET["f"])
+                        {
+                            case "mn":
+	                            $search_stmt = "SELECT * FROM p39_users WHERE name LIKE ?";
+                                break;
+
+                            case "jt":
+	                            $search_stmt = "SELECT * FROM p39_users WHERE job_title LIKE ?";
+                                break;
+                        }
+                        $search_result = Search($conn, NULL, @$search_stmt);
+	                    @$search_result_count = $search_result->num_rows;
+	                    if (@$search_result_count > 0)
+	                    {
+		                    $n = 1;
+		                    while ($search_row = $search_result->fetch_assoc()) { ?>
+                                <div class="member-box">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h4>رقم العضو:
+                                                <span class="member-number">
+                                                <?=$n?>
+                                            </span>
+                                            </h4>
+                                            <h4>اسم العضو: د.
+                                                <span class="member-name">
+                                                <?= $search_row["name"] ?>
+                                            </span>
+                                            </h4>
+                                        </div>
+                                        <div class="col">
+                                            <form method="post" action="update_member.php">
+                                                <button class="btn-basic">تعديل بيانات العضو</button>
+                                                <input type="hidden" name="user_id" value="<?=$search_row['user_id']?>">
+                                            </form>
+                                            <button class="btn-basic member-details-btn">تفاصيل العضو</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="member-details deactive">
+                                        <div class="row">
+                                            <div class="col">
+                                                <img src="<?=$search_row['image']?>" alt="" class="member-image" />
+                                            </div>
+                                            <div class="col">
+                                                <!--                                            <h4>نوع العضو: عضو مجلس</h4>-->
+                                                <h4>الاسم : <?=$search_row["name"]?></h4>
+                                                <!--                                            <h4>رقم العضو : 1</h4>-->
+                                                <!--                                            <h4>رقم تشكيل المجلس المنضم له العضو:4</h4>-->
+                                                <!--                                            <h4>رقم التليفون: 01102465132</h4>-->
+                                                <h4>الايميل: <?=$search_row["email"]?></h4>
+                                                <h4>المسمى الوظيفي: <?=$search_row["job_title"]?></h4>
+                                                <h4>الفئة الوظيفية: <?=$job_types[$search_row["job_type_id"]]?></h4>
+                                                <h4>الدرجة الوظيفية: <?=$job_ranks[$search_row["job_rank_id"]]?></h4>
+                                                <h4>القسم العلمي: <?=$departments[$search_row["department_id"]]?></h4>
+                                                <h4>حالة العضو: <?=$search_row["is_enabled"] == 1 ? "مفعل" : "غير مفعل"?></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+		                    <?php $n += 1;
+                            }
+	                    } else { ?>
+                            <main id="empty" class="empty-member">
+                                <h4>لا يوجد أعضاء الآن</h4>
+                            </main>
+                        <?php }
+                    else: ?>
+                        <div class='current-meeting'>
+                            <main id='empty' class='empty-meeting'>
+                                <h4>عذرًا، لا يوجد أعضاء تطابق البحث</h4>
+                            </main>
+                        </div>
+                    <?php endif; ?>
                     </div>
                 </div>
 
